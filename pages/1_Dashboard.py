58d45f88ef01ba58dacaa88c44a564ba7442329b
Gowthaman <gowthamanseenivasan0@gmail.com>
2026-06-20 16:04:40 +0530

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -34,7 +34,7 @@ def load_data():
         / "parking_violations.csv"
     )
 
-    pd.read_csv(csv_path, nrows=50000)
+    df = pd.read_csv(csv_path, nrows=50000)
 
     def extract_primary_violation(x):
         try:
