3314fc62cef29c22a2ef00a47f7ca71ad6bdec2c
Gowthaman <gowthamanseenivasan0@gmail.com>
2026-06-20 16:04:56 +0530

    Update 2_Heatmap.py

--- pages/2_Heatmap.py
+++ pages/2_Heatmap.py
@@ -57,7 +57,7 @@ def load_data():
         / "parking_violations.csv"
     )
 
-    pd.read_csv(csv_path, nrows=50000)
+    df = pd.read_csv(csv_path, nrows=50000)
 
     df = df.dropna(
         subset=[
