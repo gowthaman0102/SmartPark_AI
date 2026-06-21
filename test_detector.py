bb3d7e4fcd6475f51afb4042af31f3bdb4e88f0f
Gowthaman <gowthamanseenivasan0@gmail.com>
2026-06-21 07:54:57 +0530

    Delete test_detector.py

--- test_detector.py
+++
@@ -1,9 +0,0 @@
-from utils.yolo_detector import detect_vehicles
-import numpy as np
-
-dummy = np.zeros((640, 640, 3), dtype=np.uint8)
-
-result = detect_vehicles(dummy)
-
-print(type(result))
-print(len(result))
